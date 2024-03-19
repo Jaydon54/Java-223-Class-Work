2d088e9a38e9e5d5eb314608fe6d4222de899aaf
Jaydon54 <106935484+Jaydon54@users.noreply.github.com>
2024-03-19 13:54:07 -0400

    Create chapter5Question55.java
    Instructions:
    We want to build a simple “English language” calculator that does the
    following:
    ❑❑ takes three inputs from the keyboard, two of them single digits
    (0 to 9)
    ❑❑ takes a char from the keyboard, representing one of fi ve operations
    from the keyboard: 1 (addition), – (subtraction), * (multiplication),
    / (division), and ^ (exponentiation)
    ❑❑ outputs the description of the operation in plain English, as well as
    the numeric result
     For instance, if the two numbers are 5 and 3, and the operation is *,
    then the output should be
     five multiplied by three is 15
    5.14 Exercises, Problems, and Projects 211
     Note that the result is given as a number, not a word.
     If the two numbers are 2 and 9, and the operation is –, then the output
    should be
     two minus nine is -7
     If the two numbers are 5 and 2, and the operation is ^, then the output
    should be
     five to the power two is 25
     Hint: to perform the exponentiation, use the pow method of the
    Math  class.
     If the two numbers are 5 and 0, and the operation is /, then the output
    should be
     Division by zero is not allowed
     Here the operation will not be performed.
     If the two numbers are 25 and 3, and the operation is 1, then the
    output should be
     Invalid number
     because 25 has two digits.
     As for the operators, they should be translated into English as follows:
     + plus
     − minus
     * multiplied by
     / divided by
     ^ to the power
     You should use the switch … case selection statement to translate the
    input values into words.
     You need to consider these special situations:
    ❑❑ For division, there is a special constraint: you cannot divide by 0,
    and you should therefore test whether the second number is 0. If
    it is 0, then you should output a message saying that you are not
    allowed to divide by 0.
    ❑❑ The “operator” is not one of the preceding fi ve operators; in that
    case, output a message saying that the operator is not a valid one.
    ❑❑ One or two of the numbers is not a valid digit; again, you should
    output a message to that effect.
    ❑❑ Hint: You can deal with these special situations in the default
    statement of the switch block and possibly use some boolean
    variables to keep track of this information, as you may need it later
    in your program

---
+++ chapter5Question55.java
@@ -0,0 +1,117 @@
+package chap5quest55;
+import java.util.Scanner;
+
+/**
+ *
+ *
+ * @author jaydo
+ */
+public class Chap5Quest55 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // Scanner + Input
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter two numbers from 0-9");
+        int num1 = scan.nextInt();
+        if (num1 > 9 || num1 < 0)
+        {
+            System.out.println("Invalid Number, enter a number from 0-9");
+            num1 = scan.nextInt();
+        }
+        
+        int num2 = scan.nextInt();
+        if (num2 > 9 || num2 < 0)
+        {
+            System.out.println("Invalid Number, enter a number from 0-9");
+            num2 = scan.nextInt();
+        }
+        
+        System.out.println("Which operation is being performed (+,-,*,/,^)");
+        char operator = scan.next().charAt(0);
+        
+        // Convert numbers to words
+        String num1Str = intToStr(num1);
+
+        String num2Str = intToStr(num2);
+        
+        // convert char to string
+        String operatorStr = String.valueOf(operator);
+        // then find out what operation is being done and do it
+        switch (operatorStr) {
+            case "+" : {
+                int sum = num1 + num2;
+                System.out.println(num1Str + " plus " + num2Str + " is " + sum);
+            }
+            case "-" : {
+                int difference = num1 - num2;
+                System.out.println(num1Str + " minus " + num2Str + " is " + difference);
+            }
+            case "*" : {
+                int product = num1 * num2;
+                System.out.println(num1Str + " multiplied by " + num2Str + " is " + product);
+            }
+            case "/" : {
+                double quotient = (double) num1 / num2;
+                if (num2 == 0){
+                    System.out.println("Error: devision by zero");
+                }
+                else{
+                    System.out.println(num1Str + " devided by " + num2Str + " is " + quotient);
+                }
+            }
+            case "^" : {
+                double pow = Math.pow(num1, num2);
+                System.out.println(num1Str + " to the power " + num2Str + " is " + pow);
+            }
+            default : {}
+        }
+        
+    }
+   
+    
+    // This Method converts int 0-9 to word
+    public static String intToStr(int num)
+    {
+        switch (num) {
+            case 0 -> {
+                return "zero";
+            }
+            case 1 -> {
+                return "one";
+            }
+            case 2 -> {
+                return "two";
+            }
+            case 3 -> {
+                return "three";
+            }
+            case 4 -> {
+                return "four";
+            }
+            case 5 -> {
+                return "five";
+            }
+            case 6 -> {
+                return "six";
+            }
+            case 7 -> {
+                return "seven";
+            }
+            case 8 -> {
+                return "eight";
+            }
+            case 9 -> {
+                return "nine";
+            }
+            default -> {
+            return " ";
+            }
+        }
+    }
+    
+}
